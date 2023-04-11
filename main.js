2ed42628d062af82cedebda0eae979cd2c4dbf71
NickZRS <nick1804js@gmail.com>
2023-04-11 14:09:53 -0300

    P1teste

---
+++ main.js
@@ -0,0 +1,22 @@
+function result(){
+    selectt = document.getElementById("selectt");
+    resut = document.getElementById("resut");
+    Real = document.getElementById("Real");
+    Dolar = document.getElementById("Dolar");
+    num1 = document.getElementById("num1");
+if(selectt=Dolar){
+result = Real * num1;
+}
+if else(selectt=Euro){
+
+    result = Real * num1;
+    }
+    else(selectt=Libras){
+
+        result = Real * num1;
+        }
+
+
+
+
+}
