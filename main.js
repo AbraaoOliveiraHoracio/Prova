18c3cd64fdb54ffc099de999ff8dfda53bd549d1
NickZRS <nick1804js@gmail.com>
2023-04-11 14:35:15 -0300

    P2teste

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-function result(){
-    selectt = document.getElementById("selectt");
-    resut = document.getElementById("resut");
-    Real = document.getElementById("Real");
-    Dolar = document.getElementById("Dolar");
-    num1 = document.getElementById("num1");
-if(selectt=Dolar){
+const Real = document.getElementById("real").value;
+
+function resul(){
+
+    if(valor=Dolar){   
 result = Real * num1;
 }
-if else(selectt=Euro){
+if else(valort=Euro){
 
     result = Real * num1;
     }
-    else(selectt=Libras){
+    else(valor=Libras){
 
         result = Real * num1;
         }
@@ -20,3 +18,9 @@ if else(selectt=Euro){
 
 
 }
+//selectt = document.getElementById("selectt");
+  //  resut = document.getElementById("resut");
+   // Real = document.getElementById("Real");
+  //  Dolar = document.getElementById("Dolar");
+  //  num1 = document.getElementById("num1");
+//if(selectt=Dolar){
