93d18f782f7531c91ce5280fea33e21d3eabdf47
NickZRS <nick1804js@gmail.com>
2023-04-11 15:37:15 -0300

    TAFUNCIONADO

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 function Calcular(){
-const result = document.getElementById("result");
+const result1 = document.getElementById("Resultado1");
+const result2 = document.getElementById("Resultado2");
+const result3 = document.getElementById("Resultado3");
 const Calcular = document.getElementById("buton");
 const Real = parseFloat(document.getElementById("Real").value);
 
@@ -8,9 +10,9 @@ var  Dolar = parseFloat(document.getElementById("num1").value);
 var  Euro  = parseFloat(document.getElementById("num2").value);
 var  Libra = parseFloat(document.getElementById("num3").value);
 
-result.innerHTML = Real / Dolar;
-result.innerHTML = Real / Libra;
-result.innerHTML = Real / Euro;
+result1.innerHTML = Real / Dolar;
+result2.innerHTML = Real / Euro;
+result3.innerHTML = Real / Libra;
 
 
 
